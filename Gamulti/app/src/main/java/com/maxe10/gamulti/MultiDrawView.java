e5ac40a576b4a1a1a5ac8463b083d5f0a94775d8
1000len-alme <amessmer@testo.de>
2017-05-10 08:03:09 +0200

    add Gamulti app to repository

---
+++ Gamulti/app/src/main/java/com/maxe10/gamulti/MultiDrawView.java
@@ -0,0 +1,178 @@
+package com.maxe10.gamulti;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PointF;
+import android.util.AttributeSet;
+import android.util.SparseArray;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.HashSet;
+
+/**
+ * Created by maxe10 on 09/05/2017.
+ */
+
+public class MultiDrawView extends View
+{
+  private static final int CIRCLE_SIZE = 75;
+
+  private SparseArray<PointF> mActivePointers;
+  private SparseArray<Path> mActivePaths;
+
+  private HashSet<Path> mTemplatePaths;
+  private Paint mPaint;
+  private int[] colors = {Color.BLUE, Color.GREEN
+        , Color.MAGENTA, Color.CYAN
+        , Color.GRAY, Color.RED, Color.DKGRAY
+        , Color.LTGRAY, Color.YELLOW };
+
+  private Paint textPaint;
+
+  public MultiDrawView(Context context, AttributeSet attrs)
+  {
+    super(context);
+    initView();
+  }
+
+  private void initView()
+  {
+    mActivePointers = new SparseArray<PointF>();
+    mActivePaths = new SparseArray<Path>();
+    mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    // set painter color to a color you like
+    mPaint.setColor(Color.BLUE);
+    mPaint.setStyle(Paint.Style.STROKE);
+    mPaint.setStrokeJoin(Paint.Join.ROUND);
+    mPaint.setStrokeWidth(6f);
+
+
+    textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    textPaint.setTextSize(20);
+
+    createTemplatePaths();
+  }
+
+  private void createTemplatePaths()
+  {
+    mTemplatePaths = new HashSet<Path>();
+
+    Path p = new Path();
+
+    p.moveTo(100, 100);
+    p.lineTo(200, 200);
+    p.lineTo(200, 1600);
+    p.lineTo(1100, 200);
+    p.lineTo(500, 600);
+
+    mTemplatePaths.add(p);
+  }
+
+  private void comparePaths()
+  {
+    Path[] pathArray = new Path[]{};
+    Path templatePath = mTemplatePaths.toArray(pathArray)[0];
+    for (int i = 0; i < mActivePaths.size(); i++)
+    {
+      Path p = mActivePaths.get(i);
+      if (p.equals(templatePath))
+      {
+        System.out.println("EQUALS");
+      }
+    }
+  }
+
+  @Override
+  public boolean onTouchEvent(MotionEvent event)
+  {
+    // get pointer index from the event object
+    int pointerIndex = event.getActionIndex();
+
+    // get pointer ID
+    int pointerId = event.getPointerId(pointerIndex);
+
+    // get masked (not specific to a pointer) action
+    int maskedAction = event.getActionMasked();
+
+    switch (maskedAction)
+    {
+      case MotionEvent.ACTION_DOWN:
+      case MotionEvent.ACTION_POINTER_DOWN:
+        // we have a new pointer. Lets add it to the list of pointers
+
+        PointF f = new PointF();
+        f.x = event.getX(pointerIndex);
+        f.y = event.getY(pointerIndex);
+        mActivePointers.put(pointerId, f);
+
+        Path newPath = new Path();
+        newPath.moveTo(f.x, f.y);
+        mActivePaths.put(pointerId, newPath);
+        break;
+
+      case MotionEvent.ACTION_MOVE:
+        for (int i = 0; i < event.getPointerCount(); i++)
+        {
+          PointF point = mActivePointers.get(event.getPointerId(i));
+          if (point != null)
+          {
+            point.x = event.getX(i);
+            point.y = event.getY(i);
+          }
+          Path path = mActivePaths.get(event.getPointerId(i));
+          if (path != null)
+          {
+            path.lineTo(event.getX(i), event.getY(i));
+          }
+        }
+        break;
+
+      case MotionEvent.ACTION_UP:
+      case MotionEvent.ACTION_POINTER_UP:
+      case MotionEvent.ACTION_CANCEL:
+        mActivePointers.remove(pointerId);
+
+        comparePaths();
+        mActivePaths.remove(pointerId);
+        break;
+    }
+
+    invalidate();
+    return true;
+  }
+
+  @Override
+  protected void onDraw(Canvas canvas)
+  {
+    super.onDraw(canvas);
+
+    // draw all pointers
+    for (int i = 0; i < mActivePointers.size(); i++)
+    {
+      mPaint.setColor(colors[i % 9]);
+
+      PointF point = mActivePointers.valueAt(i);
+      if (point != null)
+      {
+        canvas.drawCircle(point.x, point.y, CIRCLE_SIZE, mPaint);
+      }
+
+      Path path = mActivePaths.valueAt(i);
+      if (path != null)
+      {
+        canvas.drawPath(path, mPaint);
+      }
+      canvas.drawText("Total pointers: " + mActivePointers.size(), 10, 40, textPaint);
+    }
+
+    mPaint.setColor(Color.BLACK);
+    for (Path p : mTemplatePaths)
+    {
+      canvas.drawPath(p, mPaint);
+    }
+  }
+}
