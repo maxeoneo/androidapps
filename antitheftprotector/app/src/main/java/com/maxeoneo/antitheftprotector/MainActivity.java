2d8a168ffc05d77e452234aa092a457fb75a8b45
Alex Meßmer <amessmer@testo.de>
2019-12-19 08:15:32 +0100

    move options dialog in menu settings

--- antitheftprotector/app/src/main/java/com/maxeoneo/antitheftprotector/MainActivity.java
+++ antitheftprotector/app/src/main/java/com/maxeoneo/antitheftprotector/MainActivity.java
@@ -11,6 +11,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.telephony.PhoneNumberUtils;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -41,15 +42,6 @@ public class MainActivity extends Activity
     // set toggleButton to state from database
     tOnOff = (ToggleButton) findViewById(R.id.tOnOff);
     tOnOff.setChecked(active);
-
-    // button set pwd
-    bSetPwd = (Button) findViewById(R.id.bSetPwd);
-    // hide set pwd button when active
-    if (active)
-    {
-      bSetPwd.setVisibility(View.GONE);
-    }
-    bSetPwd.setOnClickListener(new OptionsOnClickListener(this));
   }
 
   @Override
@@ -58,6 +50,15 @@ public class MainActivity extends Activity
     // Inflate the menu; this adds items to the action bar if it is present.
     getMenuInflater().inflate(R.menu.main, menu);
     return true;
+  }
+
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.action_settings:
+        createOptionsDialog();
+        return true;
+    }
+    return false;
   }
 
   /**
@@ -175,5 +176,230 @@ public class MainActivity extends Activity
     dataSource.close();
 
     return pwd;
+  }
+
+  private void createOptionsDialog()
+  {
+    // custom dialog
+    final Dialog dialog = new Dialog(this);
+    dialog.setContentView(R.layout.options_dialog);
+    dialog.setTitle(R.string.bOptions);
+
+    // set the custom dialog components
+    final EditText oldPwd = (EditText) dialog
+        .findViewById(R.id.oldPwd);
+
+    final EditText newPwd = (EditText) dialog
+        .findViewById(R.id.newPwd);
+    final EditText repeatNewPwd = (EditText) dialog
+        .findViewById(R.id.repeatNewPwd);
+
+    final ToggleButton tSendLoc = (ToggleButton) dialog
+        .findViewById(R.id.tSendLoc);
+    final EditText phoneNumber = (EditText) dialog
+        .findViewById(R.id.phoneNumber);
+
+    // get old PWD from Database
+    dataSource.open();
+    final String oldPwdString = dataSource.getPwd();
+    final String pNumber = dataSource.getPhonenumber();
+    final boolean sendLoc = dataSource.isSendLocation();
+    dataSource.close();
+
+    if (oldPwdString != "")
+    {
+      oldPwd.setVisibility(View.VISIBLE);
+    }
+    else
+    {
+      oldPwd.setVisibility(View.GONE);
+    }
+
+    // get options from database
+    phoneNumber.setText(pNumber);
+    tSendLoc.setChecked(sendLoc);
+    if (sendLoc)
+    {
+      phoneNumber.setVisibility(View.VISIBLE);
+    }
+    else
+    {
+      phoneNumber.setVisibility(View.GONE);
+    }
+
+    // set method which is called when user clicks toggle button
+    tSendLoc.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+    {
+
+      public void onCheckedChanged(CompoundButton buttonView,
+                                   boolean isChecked)
+      {
+
+        if (isChecked)
+        {
+          if (locationPermissionsGranted())
+          {
+            phoneNumber.setVisibility(View.VISIBLE);
+          }
+          else
+          {
+            tSendLoc.setChecked(false);
+          }
+        }
+        else
+        {
+          phoneNumber.setVisibility(View.GONE);
+        }
+      }
+    });
+
+    Button bSave = (Button) dialog.findViewById(R.id.bSave);
+
+    // if button is clicked, close the custom dialog
+    bSave.setOnClickListener(new View.OnClickListener()
+    {
+      @Override
+      public void onClick(View v)
+      {
+
+        // oldPwd must be right
+        if (oldPwdString == null
+            || oldPwdString.equals(oldPwd.getText()
+            .toString()))
+        {
+
+          // min 4 numbers
+          if (newPwd.getText().length() >= 4)
+          {
+
+            // newPwd and repeatNewPwd must be the same and
+            if (newPwd
+                .getText()
+                .toString()
+                .equals(repeatNewPwd.getText()
+                    .toString()))
+            {
+
+              // save options
+              dataSource.open();
+
+              // save all options (also when phonenumber
+              // is "")
+
+              String number = PhoneNumberUtils
+                  .formatNumber(phoneNumber.getText()
+                      .toString());
+              dataSource.saveOptions(newPwd.getText()
+                      .toString(), tSendLoc.isChecked(),
+                  number);
+
+              dataSource.close();
+
+              // close dialog
+              dialog.dismiss();
+
+            }
+            else
+            {
+              // pwds are not the same
+              System.out.println("PWDS are not the same");
+              // Show message
+              Toast toast = Toast.makeText(context,
+                  R.string.emNewAndRepeatedEquals,
+                  Toast.LENGTH_SHORT);
+              toast.show();
+            }
+          }
+          else
+          {
+
+            // when new pwd and repeated new pwd are empty
+            // save only the other two things
+            if (newPwd.getText().toString().equals("")
+                || repeatNewPwd.getText().toString()
+                .equals(""))
+            {
+
+              String number = PhoneNumberUtils
+                  .formatNumber(phoneNumber.getText()
+                      .toString());
+
+              dataSource.open();
+              dataSource.setSendLocation(tSendLoc
+                  .isChecked());
+              dataSource.setPhonenumber(number);
+              dataSource.close();
+
+              // close dialog
+              dialog.dismiss();
+            }
+            else
+            {
+
+              // pwds are to short
+              System.out.println("PWDS are to short");
+              // Show message
+              Toast toast = Toast.makeText(context,
+                  R.string.emPinToShort,
+                  Toast.LENGTH_SHORT);
+              toast.show();
+            }
+          }
+        }
+        else
+        {
+          // old pwd is not right
+          System.out
+              .println("old PWD is not right - database: "
+                  + oldPwdString
+                  + " your entered: "
+                  + oldPwd.getText().toString());
+          // Show message
+          Toast toast = Toast.makeText(context,
+              R.string.emOldPinNotRight,
+              Toast.LENGTH_SHORT);
+          toast.show();
+        }
+      }
+    });
+
+    dialog.show();
+  }
+
+  private boolean locationPermissionsGranted()
+  {
+    boolean granted = false;
+
+    final boolean fineLocationAccessDeclined = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED;
+    final boolean coarseLocationAccessDeclined = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED;
+    if (fineLocationAccessDeclined || coarseLocationAccessDeclined)
+    {
+
+      // Permission is not granted
+      // Should we show an explanation?
+      if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)
+          || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION))
+      {
+        // Show an explanation to the user *asynchronously* -- don't block
+        // this thread waiting for the user's response! After the user
+        // sees the explanation, try again to request the permission.
+      }
+      else
+      {
+        // No explanation needed; request the permission
+        ActivityCompat.requestPermissions(this,
+            new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1
+        );
+
+        // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+        // app-defined int constant. The callback method gets the
+        // result of the request.
+      }
+    }
+    else
+    {
+      granted = true;
+    }
+    return granted;
   }
 }
