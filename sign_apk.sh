3baf1cc5e261a5a3f818527715f57be3c7cb8904
Alex Meßmer <amessmer@testo.de>
2021-07-16 22:53:54 +0200

    fix script

--- sign_apk.sh
+++ sign_apk.sh
@@ -13,8 +13,8 @@ fi
 cd $appname/app/build/outputs/apk/release
 
 eval "KEYPASS=\"\$$appname\""
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $KEYSTORE -storepass $KEYSTORE_PASSWORD -keypass $KEYPASS app-release-signed.apk $appname
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $KEYSTORE -storepass $KEYSTORE_PASSWORD -keypass $KEYPASS app-release-unsigned.apk $appname
 
 
 SDK_VERSION=$(ls -t ${ANDROID_SDK_ROOT}/build-tools/ | head -n 1)
-${ANDROID_SDK_ROOT}/build-tools/${SDK_VERSION}/zipalign -p -f -v 4 app-release-signed.apk $appname.apk
+${ANDROID_SDK_ROOT}/build-tools/${SDK_VERSION}/zipalign -p -f -v 4 app-release-unsigned.apk $appname.apk
