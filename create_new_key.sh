14e3714b7b1c10798936d6c1698fe0d527e67afb
Alex Meßmer <amessmer@testo.de>
2021-07-16 23:32:29 +0200

    fix signing tool

--- create_new_key.sh
+++ create_new_key.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 appname=""
-password=""
 
-if [ $# == 2 ]
+if [ $# == 1 ]
   then
     appname=$1
-    password=$2
   else
-    echo "Wrong call: exactle two parameters needed"
-    echo "First one is appname"
-    echo "Second one is password"
+    echo "Wrong call: exactle one parameter appname needed"
     exit
 fi
 
-keytool -genkey -alias $appname -keystore $KEYSTORE -storetype PKCS12 -keyalg RSA -storepass $KEYSTORE_PASSWORD -keypass $password -validity 365000 -keysize 2048
+keytool -genkey -alias $appname -keystore $KEYSTORE -storetype PKCS12 -keyalg RSA -storepass $KEYSTORE_PASSWORD -validity 365000 -keysize 2048
+keytool -keypasswd -keystore $KEYSTORE
