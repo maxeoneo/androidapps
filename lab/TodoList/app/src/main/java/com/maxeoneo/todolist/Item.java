7bcbae4bce39a88805d0e52e5e2ffcb7f2b35389
Maxe Oneo <maxeoneo@gmail.com>
2021-07-23 21:42:13 +0200

    move exercise projects into lab folder

---
+++ lab/TodoList/app/src/main/java/com/maxeoneo/todolist/Item.java
@@ -0,0 +1,84 @@
+package com.maxeoneo.todolist;
+
+import java.util.GregorianCalendar;
+
+import android.location.Address;
+
+
+public class Item {
+	private long id;
+	private String name;
+	private GregorianCalendar deadline;
+	private boolean done;
+	private Address gpsLocation;
+	
+	public Item(long id, String name) {
+		this.id = id;
+		this.name = name;
+		this.done = false;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public GregorianCalendar getDeadline() {
+		return deadline;
+	}
+	
+	public String getDeadlineAsString() {
+		int intHour = deadline.get(GregorianCalendar.HOUR_OF_DAY);
+		int intMin = deadline.get(GregorianCalendar.MINUTE);
+		int intMonth = deadline.get(GregorianCalendar.MONTH) + 1;
+		int intDay = deadline.get(GregorianCalendar.DAY_OF_MONTH);
+		
+		String strHour = intHour + "";
+		String strMin = intMin + "";
+		String strMonth = intMonth + "";
+		String strDay = intDay + "";
+		
+		if (intHour < 10) {
+			strHour = "0" + intHour;
+		}
+		if (intMin < 10) {
+			strMin = "0" + intMin;
+		}
+		if (intMonth < 10) {
+			strMonth = "0" + intMonth;
+		}
+		if (intDay < 10) {
+			strDay = "0" + intDay;
+		}
+		
+		return deadline.get(GregorianCalendar.YEAR) + "-" + strMonth 
+				+ "-" + strDay + " " + strHour + ":" + strMin;	
+	}
+	
+	public void setDeadline(GregorianCalendar deadline) {
+		this.deadline = deadline;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setDone(boolean done) {
+		this.done = done;
+	}
+	public boolean getDone() {
+		return done;
+	}
+	
+	public Address getGpsLocation() {
+		return gpsLocation;
+	}
+
+	public void setGpsLocation(Address gpsLocation) {
+		this.gpsLocation = gpsLocation;
+	}
+
+	public String toString() {
+		return name;
+	}
+	
+}
