74e737ea9ae87311d2d9de1ad63e6e131f8e19be
Alex Meßmer <maxeoneo@gmail.com>
2025-11-19 16:11:05 +0100

    update signing script to sign aab files

--- scripts/sign_apk.sh
+++ scripts/sign_apk.sh
@@ -16,11 +16,13 @@ else
   exit
 fi
 
-cd $appname/app/build/outputs/apk/release
+# cd $appname/app/build/outputs/apk/release
+cd $appname/app/build/outputs/aab/release
 
 eval "KEYPASS=\"\$$appname\""
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $KEYSTORE -storepass $KEYSTORE_PASSWORD -keypass $KEYPASS app-release-unsigned.apk $appname
+# jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $KEYSTORE -storepass $KEYSTORE_PASSWORD -keypass $KEYPASS app-release-unsigned.apk $appname
+jarsigner -verbose -sigalg SHA256withRSA -digestalg SHA-256 -keystore xample.jks app-release.aab $appname  
+keytool -printcert -jarfile my-signed-binary.aab
 
-
-SDK_VERSION=$(ls -t ${ANDROID_SDK_ROOT}/build-tools/ | head -n 1)
-${ANDROID_SDK_ROOT}/build-tools/${SDK_VERSION}/zipalign -p -f -v 4 app-release-unsigned.apk $appname.apk
+#SDK_VERSION=$(ls -t ${ANDROID_SDK_ROOT}/build-tools/ | head -n 1)
+#${ANDROID_SDK_ROOT}/build-tools/${SDK_VERSION}/zipalign -p -f -v 4 app-release-unsigned.apk $appname.apk
